9037fee4eccd5c3c097a139f9ae64412a78b5aff
slaterOwen <slaterowen@hotmail.com>
2021-04-03 02:20:12 -0700

    Second commit

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,7 +1,8 @@
-import React from 'react'
-import Table from './Table'
+import React, {useState} from 'react';
+import Table from './Table';
 
-const characters = [
+function MyApp() {
+  const [characters, setCharacters] = useState([
       {
         name: 'Charlie',
         job: 'Janitor',
@@ -12,20 +13,26 @@ const characters = [
       },
       {
         name: 'Dee',
-        job: 'Aspring actress',
+        job: 'Aspiring actress',
       },
       {
         name: 'Dennis',
         job: 'Bartender',
-      },
-];
+      }
+    ]);  
 
-function MyApp() { 
-    return ( 
-      <div className="container">
-        <Table characterData={characters} />
-      </div> 
-    );  
-}   
+    function removeOneCharacter (index) {
+      const updated = characters.filter((character, i) => {
+          return i !== index
+        });
+        setCharacters(updated);
+      }
+
+      return (
+        <div className="container">
+          <Table characterData={characters} removeCharacter={removeOneCharacter} />
+        </div>
+      )
+}
 
 export default MyApp;
--- src/Table.js
+++ src/Table.js
@@ -6,6 +6,7 @@ function TableHeader()  {
       <tr>
         <th>Name</th>
         <th>Job</th>
+        <th>Remove</th>
       </tr>
     </thead>
   );
@@ -15,9 +16,12 @@ function TableBody(props) {
   const rows = props.characterData.map((row, index) => {
     return (
       <tr key={index}>
-        <td>{row.name}</td>         
-        <td>{row.job}</td>
-      </tr>
+  <td>{row.name}</td>
+  <td>{row.job}</td>
+  <td>
+    <button onClick={() => props.removeCharacter(index)}>Delete</button>
+  </td>
+</tr>
     );
    }
   );
@@ -32,7 +36,7 @@ function Table(props) {
     return (
       <table>
         <TableHeader />
-        <TableBody characterData={props.characterData} />
+        <TableBody characterData={props.characterData} removeCharacter={props.removeCharacter}/>
       </table>
     )
 }
