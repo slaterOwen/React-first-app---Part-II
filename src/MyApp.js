de9fddb5f36043f7023daf8f2f3f46f1efa82a9f
slaterOwen <slaterowen@hotmail.com>
2021-04-08 23:22:49 -0700

    Final update to frontend linking assignment

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,48 +1,66 @@
 import Table from './Table';
 import Form from './Form';
 import axios from 'axios';
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 
 
 function MyApp() {
-  const [characters, setCharacters] = useState([]);  
+  const [characters, setCharacters] = useState([]);
 
-    function removeOneCharacter (index) {
+  async function removeOneCharacter(index) {
+    const response = await axios.delete('http://localhost:5000/users/' + characters[index].id)
+    console.log(response)
+    if (response.status == 204) {
       const updated = characters.filter((character, i) => {
-          return i !== index
-        });
-        setCharacters(updated);
-      }
-      
-      function updateList(person) {
-        setCharacters([...characters, person]);
-      }
+        return i !== index
+      });
+      setCharacters(updated);
+    }
+  }
 
-      async function fetchAll(){
-        try {
-           const response = await axios.get('http://localhost:5000/users');
-           return response.data.users_list;     
-        }
-        catch (error){
-           //We're not handling errors. Just logging into the console.
-           console.log(error); 
-           return false;         
-        }
-     }
+  function updateList(person) {
+    makePostCall(person).then(result => {
+      if (result)
+        setCharacters([...characters, result]);
+    });
+  }
 
-     useEffect(() => {
-      fetchAll().then( result => {
-         if (result)
-            setCharacters(result);
-       });
-   }, [] );
-      
-      return (
-        <div className="container">
-          <Table characterData={characters} removeCharacter={removeOneCharacter} />
-          <Form handleSubmit={updateList} />
-        </div>
-      )
+  async function fetchAll() {
+    try {
+      const response = await axios.get('http://localhost:5000/users');
+      return response.data.users_list;
+    }
+    catch (error) {
+      //We're not handling errors. Just logging into the console.
+      console.log(error);
+      return false;
+    }
+  }
+
+  async function makePostCall(person) {
+    try {
+      const response = await axios.post('http://localhost:5000/users', person);
+      return response.data;
+    }
+    catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+
+  useEffect(() => {
+    fetchAll().then(result => {
+      if (result)
+        setCharacters(result);
+    });
+  }, []);
+
+  return (
+    <div className="container">
+      <Table characterData={characters} removeCharacter={removeOneCharacter} />
+      <Form handleSubmit={updateList} />
+    </div>
+  )
 }
 
 export default MyApp;
--- src/Table.js
+++ src/Table.js
@@ -1,10 +1,11 @@
 import React from 'react'
-function TableHeader()  {
+function TableHeader() {
   return (
     <thead>
       <tr>
         <th>Name</th>
         <th>Job</th>
+        <th>ID</th>
         <th>Remove</th>
       </tr>
     </thead>
@@ -14,27 +15,28 @@ function TableBody(props) {
   const rows = props.characterData.map((row, index) => {
     return (
       <tr key={index}>
-  <td>{row.name}</td>
-  <td>{row.job}</td>
-  <td>
-    <button onClick={() => props.removeCharacter(index)}>Delete</button>
-  </td>
-</tr>
+        <td>{row.name}</td>
+        <td>{row.job}</td>
+        <td>{row.id}</td>
+        <td>
+          <button onClick={() => props.removeCharacter(index)}>Delete</button>
+        </td>
+      </tr>
     );
-   }
+  }
   );
   return (
-      <tbody>
-        {rows}
-       </tbody>
-   );
+    <tbody>
+      {rows}
+    </tbody>
+  );
 }
 function Table(props) {
-    return (
-      <table>
-        <TableHeader />
-        <TableBody characterData={props.characterData} removeCharacter={props.removeCharacter}/>
-      </table>
-    )
+  return (
+    <table>
+      <TableHeader />
+      <TableBody characterData={props.characterData} removeCharacter={props.removeCharacter} />
+    </table>
+  )
 }
 export default Table;
